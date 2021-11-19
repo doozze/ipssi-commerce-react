40aa7c012df43e6163341601e0e8319d906eccda
christophe leonardi <c.leonardi@live.fr>
2021-11-19 11:44:00 +0100

    change input to textarea

--- src/components/TextForm/index.js
+++ src/components/TextForm/index.js
@@ -3,7 +3,7 @@ import './index.css'
 export const TextForm = (props) => {
     return (<div className="textForm">
         <label for={props.inputId} id={props.labelId}>{props.labelValue}</label>
-        <input type = "text" id={props.inputId}/>
+        <textarea type = "text" id={props.inputId}></textarea>
     </div>)
 
 }
