bc144f2c1d86ca5b60a8517a10349ee1a38fb2e6
Harold Doumbe <doumbelachemiz@gmail.com>
2021-11-19 12:31:14 +0100

    --listCodePromo with useState

--- src/components/ListCodePromo/index.js
+++ src/components/ListCodePromo/index.js
@@ -1,5 +1,25 @@
 import './index.css'
+import {LigneCodePromo} from "../LigneCodePromo";
+import {useState} from "react";
 
 export const ListCodePromo= ({props}) => {
 
+    const [dataCode] = useState(props.dataPromoCode);
+
+    return (<div>
+        <table className="tabCodePromo">
+            <thead>
+                <tr className="tabCodePromoHead">
+                    <td>Nom Code Promo</td>
+                    <td>Reduction (%)</td>
+                </tr>
+            </thead>
+            <tbody>
+            {dataCode.map((codePromo)=> (
+                <LigneCodePromo nomCode={codePromo.code} reductionCode={codePromo.pourcentageReduction} nomCodeId={}
+                reductionCodeId={}/>
+            ))}
+            </tbody>
+        </table>
+    </div>)
 }
