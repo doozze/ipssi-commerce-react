a03711eafaef10a63d411e70831915aa44f64e00
Harold Doumbe <doumbelachemiz@gmail.com>
2021-11-19 09:35:57 +0100

    --textContainer

--- src/App.js
+++ src/App.js
@@ -4,24 +4,24 @@ import {
   Route
 } from "react-router-dom";
 
-import { Home } from './components/Home';  
+import { Home } from './components/Home';
 import { Counter } from './components/Counter';
 
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={
-          <Home />
-        }/>
-        
-        <Route path='/counter' element={
-          <Counter />
-        }/>
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path='/' element={
+            <Home />
+          }/>
+
+          <Route path='/counter' element={
+            <Counter />
+          }/>
+        </Routes>
+      </Router>
   );
 }
 
