8f9ebeb4a9e2e9b9d01ed4cb0f19d9fdc4c486d0
christophe leonardi <c.leonardi@live.fr>
2021-11-19 11:36:43 +0100

    remove balise p

--- src/components/TextForm/index.js
+++ src/components/TextForm/index.js
@@ -2,8 +2,8 @@ import './index.css'
 
 export const TextForm = (props) => {
     return (<div className="textForm">
-        <p><label for={props.inputId} id={props.labelId}>{props.labelValue}</label></p>
-        <p><input type = "text" id={props.inputId}/></p>
+        <label for={props.inputId} id={props.labelId}>{props.labelValue}</label>
+        <input type = "text" id={props.inputId}/>
     </div>)
 
 }
