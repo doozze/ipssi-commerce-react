b40c9246cc14699502b9466f1b5e00071ac286a2
Harold Doumbe <doumbelachemiz@gmail.com>
2021-11-19 10:23:52 +0100

    --creerCodePromo

--- src/components/CreerCodePromo/index.js
+++ src/components/CreerCodePromo/index.js
@@ -1,7 +1,19 @@
 import './index.css'
 
 import {Button} from "../Button";
+import {H3Title} from "../h3Title";
+import {TextForm} from "../TextForm";
 
-export const CreerCodePromo= ({props}) => {
+export const CreerCodePromo= (props) => {
 
+    return(
+        <div className="CreerCodePromo">
+            <div> <H3Title content="Créer Code Promo"/> </div>
+            <div className="FormCodePromo">
+                <TextForm inputId="nomCodeInput" labelId="nomCodeLabel" labelValue="Nom Code Promo"/>
+                <TextForm inputId="reducCodeInput" labelId="reducCodeLabel" labelValue="Réduction (%)"/>
+                <Button value="Créer code Promo"/>
+            </div>
+        </div>
+    )
 }
