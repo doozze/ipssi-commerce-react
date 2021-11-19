60cb25b0969e0404d2a1bb88e61eb45cd57b7f09
Harold Doumbe <doumbelachemiz@gmail.com>
2021-11-19 12:37:17 +0100

    --listCodePromo addTitles

--- src/components/ListCodePromo/index.js
+++ src/components/ListCodePromo/index.js
@@ -1,12 +1,17 @@
 import './index.css'
 import {LigneCodePromo} from "../LigneCodePromo";
 import {useState} from "react";
+import {H2Title} from "../h2Title";
+import {H3Title} from "../h3Title";
 
 export const ListCodePromo= ({props}) => {
 
     const [dataCode] = useState(props.dataPromoCode);
 
-    return (<div>
+    return (
+        <div className="ListCodePromo">
+            <H2Title className="titleListCode" content="Codes Promo"/>
+            <H3Title className="subTitleListCode" content="Liste des Codes Promo"/>
         <table className="tabCodePromo">
             <thead>
                 <tr className="tabCodePromoHead">
