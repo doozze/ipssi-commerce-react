4881d5a7a71f249e2dcf84f4e12a55c182d870eb
christophe leonardi <c.leonardi@live.fr>
2021-11-19 11:09:43 +0100

    productInfosContainer

--- src/components/productInfosContainer/index.js
+++ src/components/productInfosContainer/index.js
@@ -1,6 +1,16 @@
 import './index.css'
+import { TextInfos } from '../TextInfos'
+import { AddToCartContainer } from '../AddToCartContainer'
+import { Button } from '../Button'
 
-export const H3Title = ({ content }) => {
+export const ProductInfosContainer = ({ product, vendor }) => {
     
-    return <h3 className="titreH3">{content}</h3>
+    return (
+        <div className="productInfosContainer">
+            <TextInfos content={product.title} value={product.textValue}/>
+            <TextInfos content={vendor.title} value={vendor.textValue}/>
+            <Button value="Contacter le vendeur"/>
+            <AddToCartContainer price={product.price}/> 
+        </div>
+    )
 }
