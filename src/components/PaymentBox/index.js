4c0b7cc7ad84b20c0e41095ec17680ff85e3ae13
Harold Doumbe <doumbelachemiz@gmail.com>
2021-11-19 15:00:42 +0100

    axios config

--- src/components/PaymentBox/index.js
+++ src/components/PaymentBox/index.js
@@ -21,20 +21,28 @@ export const PaymentBox = (props) => {
                 <div className="infoPayDiv" id="infosCB">
                     <div>
                         <div>
-                            <p> <TextForm/> </p>
-                            <p> <TextForm/> <TextForm/> </p>
+                            <p> <TextForm inputId="inputCardName" labelId="labelInputCardName" labelValue="Nom figurant sur la carte"/> </p>
+                            <p> <TextForm inputId="inputCardNumber" labelId="labelInputCardNumber" labelValue="Numéro de carte"/> <TextForm inputId="inputCardCCV" labelId="labelInputCardCCV" labelValue="Code CCV"/> </p>
                         </div>
                         <div>
-                            <H3Title />
-                            <Button />
-                            <TextContainer />
+                            <H3Title content="Total Commande (€)"/>
+                            <Button value = "Effectuer le paiement"/>
+                            <TextContainer textValue="En effectuant la commande, j'accepte les CGU"/>
                         </div>
                     </div>
                 </div>
-                <div className="infoPayDiv" id="infosVir"></div>
-                <div className="infoPayDiv" id="infosPP"></div>
-                <div className="infoPayDiv" id="infosCC"></div>
-                <div className="infoPayDiv" id="infosBT"></div>
+                <div className="infoPayDiv" id="infosVir">
+
+                </div>
+                <div className="infoPayDiv" id="infosPP">
+
+                </div>
+                <div className="infoPayDiv" id="infosCC">
+
+                </div>
+                <div className="infoPayDiv" id="infosBT">
+
+                </div>
             </div>
         </div>
     )
