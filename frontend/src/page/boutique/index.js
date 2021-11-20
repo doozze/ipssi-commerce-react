8e08c03236b2bc0d11d6bf53b3a554c7cd03207d
christophe leonardi <c.leonardi@live.fr>
2021-11-20 11:53:03 +0100

    update page connexion & boutique

--- frontend/src/page/boutique/index.js
+++ frontend/src/page/boutique/index.js
@@ -6,16 +6,14 @@ import {useEffect} from "react";
 
 export const PageBoutique = ({ productList }) => {
 
-    let vignettes
     //let products = productList
     const {products, setProducts} = useAPIContext();
     //products.map( product => { vignettes += <VignetteProduct urlProduct="" urlImage={product.photo} productName={product.nom_produit} vendor={product.vendeur} price={product.prix}/> })
 
     useEffect(()=> {console.log(products)},[products])
-
     return(
         <section className="PageBoutique">
-            <H1Title content={productList.pageTitle}/>
+            <H1Title content={productList}/>
             <div className="vignetteContainer">
                 {
                     products && products.map( product =>  <VignetteProduct urlImage={product.photo} productName={product.nom_produit} vendor={product.vendeur} price={product.prix}/> )
--- frontend/src/page/connexion/index.js
+++ frontend/src/page/connexion/index.js
@@ -1,15 +1,48 @@
 import './index.css'
-import { H1Title } from '../h1Title'
+import { H1Title } from '../../components/h1Title'
 import { ConnexionForm } from '../../components/LogSubscribe'
 
-export const PageConnexion = ({ props, connexion, inscription }) => {
+export const PageConnexion = ({ pageTitle }) => {
 
+    const formulaire = {
+        connexion: {
+            typeForm: "connexion",
+            login: {
+                inputId: "login",
+                labelId: "login",
+                labelValue: "mail",
+                type: "text",
+            },
+            password: {
+                inputId: "password",
+                labelId: "password",
+                labelValue: "Mot de passe",
+                type: "password",
+            },
+        },
+        inscription: {
+            typeForm: "inscription",
+            login: {
+                inputId: "login",
+                labelId: "login",
+                labelValue: "mail",
+                type: "text",
+            },
+            password: {
+                inputId: "password",
+                labelId: "password",
+                labelValue: "Mot de passe",
+                type: "password",
+            },
+        }
+    }
+    console.log(formulaire)
     return(
         <section className="pageConnexion">
-            <H1Title content={props.pageTitle}/>
+            <H1Title content={pageTitle}/>
             <div className="formContainer">
-                <ConnexionForm props={props.connexion}/>
-                <ConnexionForm props={props.inscription}/>
+                <ConnexionForm props={formulaire.connexion}/>
+                <ConnexionForm props={formulaire.inscription}/>
             </div>
         </section>
         
