b520b229f2d34e813ec7aba9fd5d710ef77d4526
christophe leonardi <c.leonardi@live.fr>
2021-11-20 12:31:49 +0100

    ajout page compte

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,9 @@ import { Home } from './components/Home';
 import { Counter } from './components/Counter';
 import { CodeP } from "./components/codePromo";
 import { PageBoutique } from "./page/boutique"
+import { PageConnexion } from "./page/connexion"
+import { PageCompte } from "./page/compte";
+import { Navigation } from "./components/Navigation";
 import './App.css';
 import {APIContextProvider} from "./contexts/APIContext";
 
@@ -16,16 +19,42 @@ function App() {
       <APIContextProvider>
         <Router>
           <Routes>
-            <Route path='/' element={
-              <PageBoutique productList="Boutique"/>
-            }/>
-
-            <Route path='/counter' element={
-              <Counter />
-            }/>
             <Route path='/boutique' element={
-              <pageBoutique />
+              <div className="page">
+                <header>
+                  <Navigation/>
+                </header>
+                <PageBoutique productList="Boutique"/>
+                <footer>
+                  <Navigation/>
+                </footer>
+              </div>
             }/>
+            <Route path='/connexion' element={
+              <div className="page">
+                <header>
+                  <Navigation/>
+                </header>
+                <PageConnexion pageTitle="Connexion" />
+                <footer>
+                  <Navigation/>
+                </footer>
+              </div>
+            }/>
+            <Route path='/compte' element={
+              <div className="page">
+                <header>
+                  <Navigation/>
+                </header>
+                <PageCompte pageTitle="Mon Compte" />
+                <footer>
+                  <Navigation/>
+                </footer>
+              </div>
+            }/>
+ 
+
+
           </Routes>
         </Router>
       </APIContextProvider>
