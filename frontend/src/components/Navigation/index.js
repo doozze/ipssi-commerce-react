5148b10acb931e8a4b24d83d242352398d24ee9f
christophe leonardi <c.leonardi@live.fr>
2021-11-20 15:51:45 +0100

    update nav

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -7,7 +7,7 @@ export const Navigation = ( {position} ) => {
             <img src="" alt="logo"/>
             <ul>
                 <li><a href="http://localhost:3000/boutique">Boutique</a></li>
-                <li><a href="http://localhost:3000/produits">Fiche produit</a></li>
+                <li><a href="http://localhost:3000/produit">Fiche produit</a></li>
                 <li><a href="http://localhost:3000/compte">Compte</a></li>
                 <li><a href="http://localhost:3000/cguv">CGUV</a></li>
                 <li><a href="http://localhost:3000/connexion">Connexion</a></li>
