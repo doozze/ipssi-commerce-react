84bd19b7c3e764be5a74e3a56fdf68f32d9fbb56
christophe leonardi <c.leonardi@live.fr>
2021-11-21 13:57:50 +0100

    ajout bouton vignette produit

--- frontend/src/components/VignetteProduct/index.js
+++ frontend/src/components/VignetteProduct/index.js
@@ -1,6 +1,7 @@
 import './index.css'
 import { ImageProduct } from '../ImageProduct'
 import { ShortInfosModule } from '../ShortInfosProduct'
+import { Button } from '../Button'
 
 export const VignetteProduct = (props) => {
 
@@ -9,6 +10,7 @@ export const VignetteProduct = (props) => {
             <a href="">
                 <ImageProduct src={props.urlImage} productName={props.productName}/>
                 <ShortInfosModule productName={props.productName} vendor={props.vendor} price={props.price}/>
+                <Button value="Voir le produit"/>
             </a>
         </div>
     )
